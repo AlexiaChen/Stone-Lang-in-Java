d0e90f94dbfd5ffe8f10281cab5fd196145a7801
MathxH Chen <brainfvck@foxmail.com>
2019-01-01 18:19:58 +0800

    re init project

--- README.md
+++
@@ -1,5 +0,0 @@
-# stone-lang-in-cpp
- Implementation of Stone Language in cpp
- 
-# dependency
-boost library-1.63.0
--- Token.cpp
+++
@@ -1,4 +0,0 @@
-#include "Token.h"
-
-const std::shared_ptr<Token> Token::EOF_TOEKN = std::make_shared<Token>(-1);
-const std::string Token::EOL_TOKEN = std::string("\\n");
